f1cdc527b5091ba2039a3043a9e38295e753f681
Chris Diggle <chrisdiggle8@outlook.com>
2024-05-10 12:51:05 +0000

    Setup axios interceptors to refresh auth tokens

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://connect-api-5644c52001aa.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
