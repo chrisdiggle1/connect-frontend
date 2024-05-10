83dfa93ca2145d11b65ef4aa0f8f28bfd5b341c2
Chris Diggle <chrisdiggle8@outlook.com>
2024-05-10 11:27:16 +0000

    Create sign in page and add styling

---
+++ src/pages/auth/SignInForm.js
@@ -0,0 +1,129 @@
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import styles from "../../styles/SignInUpForm.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import appStyles from "../../App.module.css";
+import axios from "axios";
+import {
+  Form,
+  Button,
+  Image,
+  Col,
+  Row,
+  Container,
+  Alert,
+} from "react-bootstrap";
+
+const SignInForm = () => {
+  const [signInData, setSignInData] = useState({
+    username: "",
+    password: "",
+  });
+  const { username, password } = signInData;
+
+  const [errors, setErrors] = useState({});
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    setSignInData({
+      ...signInData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("/dj-rest-auth/login/", signInData);
+      history.push("/home");
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
+
+  return (
+    <Row className={styles.Row}>
+      <Col md={12} className={styles.CenteredLogoContainer}>
+        <Image
+          src="https://res.cloudinary.com/dseyqu8cu/image/upload/v1715334067/logo_q9kcdf.png"
+          className={styles.Logo}
+          alt="Logo"
+        />
+      </Col>
+
+      <Col md={6} className={`my-auto py-2 p-md-2 ${styles.FormCol}`}>
+        <Container className={`${appStyles.Content} p-4`}>
+          <h1 className={styles.Header}>Sign in</h1>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="username">
+              <Form.Label className="d-none">Username</Form.Label>
+              <Form.Control
+                className={styles.Input}
+                type="text"
+                placeholder="Username"
+                name="username"
+                value={username}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {errors.username?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+
+            <Form.Group controlId="password">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control
+                className={styles.Input}
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={password}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {errors.password?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+
+            <Button
+              className={`${btnStyles.Button} ${btnStyles.Bright} ${btnStyles.Wide}`}
+              type="submit"
+            >
+              Sign In
+            </Button>
+            {errors.non_field_errors?.map((message, idx) => (
+              <Alert variant="warning" key={idx} className="mt-3">
+                {message}
+              </Alert>
+            ))}
+          </Form>
+        </Container>
+
+        <Container className={`mt-3 ${appStyles.Content}`}>
+          <Link className={styles.Link} to="/signup">
+            Don't have an account? <span>Sign up</span>
+          </Link>
+        </Container>
+      </Col>
+
+      <Col
+        md={6}
+        className={`my-auto d-none d-md-block p-2 ${styles.SignInCol}`}
+      >
+        <Image
+          className={`${appStyles.FillerImage} ${styles.SignInImage}`}
+          src={
+            "https://res.cloudinary.com/dseyqu8cu/image/upload/v1715338550/signin-image_mto4sp.jpg"
+          }
+          alt="Sign in visual"
+        />
+      </Col>
+    </Row>
+  );
+};
+
+export default SignInForm;
